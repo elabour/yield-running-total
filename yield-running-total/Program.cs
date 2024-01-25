a726b631124b4ce7c1f18d53bfc87d1cc7f7b30b
Abdulmottaleb Elabour <aelabour@outlook.com>
2024-01-25 10:36:26 +0200

    Add project files.

---
+++ yield-running-total/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yield_running_total
+{
+    internal class Program
+    {
+      
+public static List<int> vals = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+public static IEnumerable<int> RunningTotal()
+        {
+            int runningTotal = 0;
+
+            foreach (int val in vals)
+            {
+                runningTotal += val;
+                yield return runningTotal;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        
+            foreach (int e in RunningTotal())
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.Read();
+
+        }
+
+       
+
+    }
+}
